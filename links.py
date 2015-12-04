34f7bf86a3bb924f4539e34d594bc43e34f2ffc3
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-03 21:34:12 -0500

    Modify python file to output json

--- links.py
+++ links.py
@@ -1,8 +1,7 @@
 #!/usr/local/bin/python3
 
-import glob
+import glob, json
 
 gifs = [g.replace('TheGIFFile/', '') for g in glob.glob("/Users/lucas/Dropbox/Public/TheGIFFile/*.gif")]
 
-for gif in gifs:
-	print('<a href="https://dl.dropboxusercontent.com/u/9296799/TheGIFFile/' + gif + '">' + gif + '</a><br>')
+print(json.dumps(gifs))
