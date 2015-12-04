565587bc624a84c800f61ccb894832c5027f7b5e
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-03 21:37:14 -0500

    Include call for Angular's $http service

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var TheGIFFileApp = angular.module('TheGIFFileApp', []);
 
-TheGIFFileApp.controller('GIFListCtrl', function ($scope) {
+TheGIFFileApp.controller('GIFListCtrl', function ($scope, $http) {
 	$http.get('gifs.json').success(function(data) {
 		$scope.gifs = data;
 	});
